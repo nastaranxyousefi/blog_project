7a15f1b4ae8df6dc0f2f91083b365bbb6af687b8
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-05-25 22:59:08 +0430

    add img tag to PostDetailView

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,4 @@ from .models import Post
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'text', 'author', 'status']
+        fields = ['title', 'text', 'author', 'status', 'cover']
